927d608a001c5d5cf058a8b845b2073ed4c01a42
Drabzee <uddeshya_kumar@ymail.com>
2020-11-22 14:19:04 +0530

    layout component created

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import axios from 'axios';
 import DataTable from '../../components/DataTable/DataTable';
-import Navbar from '../../components/Navbar/Navbar';
 import StatusCards from '../../components/StatusCards/StatusCards';
 import style from './Home.module.css';
+import Layout from '../../components/Layout/Layout';
 
 const Home = () => {
 
@@ -34,15 +34,16 @@ const Home = () => {
       });
   }, [shipmentData]);
 
-  return shipmentData.length && filter.status ? (
-    <div className="home">
-      <Navbar />
-      <StatusCards filter={filter} setFilter={setFilter} />
-      <div className={style.container}>
-        <DataTable shipmentData={shipmentData} status={filter.status} />
-      </div>
-    </div>
-  ) : error ? <p>{error}</p> : <div />
+  return (
+    <Layout>
+      { shipmentData.length && filter.status ? (
+        <Fragment>
+          <StatusCards filter={filter} setFilter={setFilter} />
+          <DataTable shipmentData={shipmentData} status={filter.status} />
+        </Fragment>
+      ) : error ? <div className={style.error}>{error}</div> : <div /> }
+    </Layout>
+  );
 }
 
 export default Home;
